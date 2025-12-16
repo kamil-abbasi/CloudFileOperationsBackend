1af2c986c9c79fbf29a5d5e364aa1e5d934cd439
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-16 23:13:35 +0100

    implemented Update and Remove methods

--- services/files/internal/files/service.go
+++ services/files/internal/files/service.go
@@ -1,6 +1,10 @@
 package files
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/config"
 	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/files/dtos"
 	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/files/entities"
@@ -29,8 +33,18 @@ func (s *FilesService) FindOne(id string) (entities.File, bool, error) {
 }
 
 func (s *FilesService) Create(dto dtos.FileCreateDto) (entities.File, error) {
-	filePath := s.config.RootPath + dto.Location + "/" + dto.Filename
-	exists, err := shared.DoesFileExist(filePath)
+	fullPath := filepath.Clean(
+		filepath.Join(
+			s.config.RootPath,
+			dto.UserId,
+			dto.Location,
+			dto.Filename,
+		),
+	)
+
+	fmt.Println(fullPath)
+
+	exists, err := shared.DoesFileExist(fullPath)
 
 	if err != nil {
 		return entities.File{}, err
@@ -44,9 +58,89 @@ func (s *FilesService) Create(dto dtos.FileCreateDto) (entities.File, error) {
 }
 
 func (s *FilesService) Update(dto dtos.FileUpdateDto) (entities.File, bool, error) {
-	return s.repository.Update(dto)
+	file, found, err := s.repository.FindOne(dto.Where.Id)
+	fullPath := filepath.Join(filepath.Join(
+		s.config.RootPath,
+		file.UserId,
+		file.Location,
+		file.Filename,
+	))
+
+	if err != nil {
+		return entities.File{}, false, err
+	}
+
+	if !found {
+		return entities.File{}, false, nil
+	}
+
+	if dto.Fields.Filename == "" {
+		dto.Fields.Filename = file.Filename
+	}
+
+	if dto.Fields.Location == "" {
+		dto.Fields.Location = file.Location
+	}
+
+	fmt.Println(dto)
+
+	newFullPath := filepath.Join(filepath.Join(
+		s.config.RootPath,
+		file.UserId,
+		dto.Fields.Location,
+		dto.Fields.Filename,
+	))
+
+	err = os.MkdirAll(filepath.Dir(newFullPath), 0755)
+
+	if err != nil {
+		return entities.File{}, false, fmt.Errorf("error while creating dirs: %v", err)
+	}
+
+	err = os.Rename(fullPath, newFullPath)
+
+	if err != nil {
+		return entities.File{}, false, fmt.Errorf("error while moving file: %v", err)
+	}
+
+	s.repository.Update(dto)
+
+	return entities.File{
+		Id:       file.Id,
+		UserId:   file.UserId,
+		Size:     file.Size,
+		Location: dto.Fields.Location,
+		Filename: dto.Fields.Filename,
+	}, true, nil
 }
 
 func (s *FilesService) Remove(id string) (entities.File, bool, error) {
-	return s.repository.Remove(id)
+	file, found, err := s.repository.FindOne(id)
+
+	if err != nil {
+		return entities.File{}, false, err
+	}
+
+	if !found {
+		return entities.File{}, false, nil
+	}
+
+	fullPath := filepath.Clean(
+		filepath.Join(
+			s.config.RootPath,
+			file.UserId,
+			file.Location,
+			file.Filename,
+		),
+	)
+
+	err = os.Remove(fullPath)
+
+	if err != nil {
+		return entities.File{}, false, err
+	}
+
+	s.repository.Remove(id)
+
+	return file, true, nil
 }
