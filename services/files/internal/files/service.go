8e74ee37035b18eabe447a4dcee102352aca8c8d
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-16 19:09:08 +0100

    added config and check for file existence in Create method

--- services/files/internal/files/service.go
+++ services/files/internal/files/service.go
@@ -1,36 +1,52 @@
 package files
 
 import (
+	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/config"
 	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/files/dtos"
 	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/files/entities"
+	errs "github.com/kamil-abbasi/CloudFileOperationsBackend/internal/files/errors"
 	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/files/interfaces"
+	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/shared"
 )
 
-type FileService struct {
+type FilesService struct {
 	repository interfaces.IFilesRepository
+	config     *config.Config
 }
 
-func NewService() *FileService {
+func NewService(config *config.Config) *FilesService {
 
-	repository := NewSQLiteRepository()
+	repository := NewSQLiteRepository(config)
 
-	return &FileService{
+	return &FilesService{
 		repository: repository,
+		config:     config,
 	}
 }
 
-func (service *FileService) FindOne(id string) (entities.File, bool, error) {
-	return service.repository.FindOne(id)
+func (s *FilesService) FindOne(id string) (entities.File, bool, error) {
+	return s.repository.FindOne(id)
 }
 
-func (service *FileService) Create(dto dtos.FileCreateDto) (entities.File, error) {
-	return service.repository.Create(dto)
+func (s *FilesService) Create(dto dtos.FileCreateDto) (entities.File, error) {
+	filePath := s.config.RootPath + dto.Location + "/" + dto.Filename
+	exists, err := shared.DoesFileExist(filePath)
+
+	if err != nil {
+		return entities.File{}, err
+	}
+
+	if exists {
+		return entities.File{}, &errs.FileAlreadyExistsError{}
+	}
+
+	return s.repository.Create(dto)
 }
 
-func (service *FileService) Update(dto dtos.FileUpdateDto) (entities.File, bool, error) {
-	return service.repository.Update(dto)
+func (s *FilesService) Update(dto dtos.FileUpdateDto) (entities.File, bool, error) {
+	return s.repository.Update(dto)
 }
 
-func (service *FileService) Remove(id string) (entities.File, bool, error) {
-	return service.repository.Remove(id)
+func (s *FilesService) Remove(id string) (entities.File, bool, error) {
+	return s.repository.Remove(id)
 }
