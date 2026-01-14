676b26f21f22c15bc32d4413fd978847613cf8cd
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2026-01-14 16:15:14 +0100

    added cache

--- services/files/internal/api/router.go
+++ services/files/internal/api/router.go
@@ -2,17 +2,18 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/patrickmn/go-cache"
 
 	v1 "github.com/kamil-abbasi/CloudFileOperationsBackend/internal/api/v1"
 	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/config"
 )
 
-func NewRouter(config *config.Config) *gin.Engine {
+func NewRouter(config *config.Config, cache *cache.Cache) *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/healthcheck")
 
-	v1.RegisterRoutes(r.Group("v1"), config)
+	v1.RegisterRoutes(r.Group("v1"), config, cache)
 
 	return r
 }
