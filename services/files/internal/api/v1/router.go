4d28da4e7fd34c170f858ac78abcb8626d62106f
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2026-01-14 16:15:24 +0100

    added cache

--- services/files/internal/api/v1/router.go
+++ services/files/internal/api/v1/router.go
@@ -2,15 +2,16 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/patrickmn/go-cache"
 
 	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/config"
 	files "github.com/kamil-abbasi/CloudFileOperationsBackend/internal/files/controllers/v1"
 )
 
-func RegisterRoutes(rg *gin.RouterGroup, config *config.Config) {
+func RegisterRoutes(rg *gin.RouterGroup, config *config.Config, cache *cache.Cache) {
 	filesRouter := rg.Group("/files")
 	{
-		controller := files.NewController(config)
+		controller := files.NewController(config, cache)
 
 		filesRouter.GET("/:id", controller.FindOne)
 
