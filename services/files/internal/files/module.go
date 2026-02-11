63e965b2ea17b3327551de86523cf2df12a6a0cb
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2026-02-11 16:59:15 +0100

    functions and structs describing module for managing files, which make it easier to write cleaner code

---
+++ services/files/internal/files/module.go
@@ -0,0 +1,33 @@
+package files
+
+import (
+	"database/sql"
+
+	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/config"
+	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/files/interfaces"
+	"github.com/patrickmn/go-cache"
+)
+
+type ModuleDeps struct {
+	Db     *sql.DB
+	Config *config.Config
+	Cache  *cache.Cache
+}
+
+type ModuleExports struct {
+	Repository *interfaces.IFilesRepository
+	Service    *FilesService
+	Controller *FilesController
+}
+
+func NewModule(deps *ModuleDeps) *ModuleExports {
+	repository := NewSQLiteRepository(deps.Config, deps.Db)
+	service := NewService(deps.Config, repository)
+	controller := NewController(deps.Config, deps.Cache, &service)
+
+	return &ModuleExports{
+		Repository: &repository,
+		Service:    &service,
+		Controller: &controller,
+	}
+}
