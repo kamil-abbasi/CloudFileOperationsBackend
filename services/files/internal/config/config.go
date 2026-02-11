43fff3200061af5c682be5c1dd957c6e929958e2
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2026-02-11 16:51:54 +0100

    refactor

--- services/files/internal/config/config.go
+++ services/files/internal/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -13,48 +13,54 @@ type Config struct {
 	UsageCacheTTL  uint32
 	LogLevel       string
 	GinMode        string
+	PgDb           string
+	PgUser         string
+	PgPassword     string
+	PgHost         string
 }
 
-func New() *Config {
-	return &Config{}
-}
+func Load() (*Config, error) {
+	config := &Config{}
 
-func (config *Config) Load() {
 	portStr := os.Getenv("PORT")
 	rootPath := os.Getenv("ROOT_PATH")
 	maxUploadBytesStr := os.Getenv("MAX_UPLOAD_BYTES")
 	usageCacheTTLStr := os.Getenv("USAGE_CACHE_TTL")
 	logLevel := os.Getenv("LOG_LEVEL")
 	ginMode := os.Getenv("GIN_MODE")
+	pgDb := os.Getenv("PG_DB")
+	pgUser := os.Getenv("PG_USER")
+	pgPassword := os.Getenv("PG_PASSWORD")
+	pgHost := os.Getenv("PG_HOST")
 
 	port, err := strconv.Atoi(portStr)
 
 	if err != nil {
-		log.Panic("PORT must be an integer")
-		return
+		return nil, fmt.Errorf("PORT must be an integer")
 	}
-
-	config.Port = uint32(port)
-	config.RootPath = rootPath
 
 	maxUploadBytes, err := strconv.Atoi(maxUploadBytesStr)
 
 	if err != nil {
-		log.Panic("MAX_UPLOAD_BYTES must be an integer")
-		return
+		return nil, fmt.Errorf("MAX_UPLOAD_BYTES must be an integer")
 	}
-
-	config.MaxUploadBytes = uint64(maxUploadBytes)
 
 	usageCacheTTL, err := strconv.Atoi(usageCacheTTLStr)
 
 	if err != nil {
-		log.Fatal("USAGE_CACHE_TTL must be an integer")
-		return
+		return nil, fmt.Errorf("USAGE_CACHE_TTL must be an integer")
 	}
 
+	config.MaxUploadBytes = uint64(maxUploadBytes)
+	config.Port = uint32(port)
+	config.RootPath = rootPath
 	config.UsageCacheTTL = uint32(usageCacheTTL)
 	config.GinMode = ginMode
 	config.LogLevel = logLevel
+	config.PgDb = pgDb
+	config.PgUser = pgUser
+	config.PgPassword = pgPassword
+	config.PgHost = pgHost
 
+	return config, nil
 }
